b49f4869e7933e63a6ea573275ae836ba5c98a97
zh <87256657@qq.com>
2021-12-02 11:15:35 +0800

    修改 路由

--- src/router/index.js
+++ src/router/index.js
@@ -1,58 +1,17 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Layout from '@/layout/index.vue'
-
 Vue.use(VueRouter)
 
-const routes = [
-  {
-    path: '/',
-    component: Layout,
-    children: [
-      // 一个轻量级的，稳健的，优雅简洁的语法高亮插件
-      {
-        path: '/prismjs',
-        component: () => import('@/views/prismjs.vue')
-      },
-      // 在页面展示一个或多个被分割的区块
-      {
-        path: '/vue-splitpane',
-        component: () => import('@/views/vue-splitpane.vue')
-      },
-      // 国际化
-      {
-        path: '/i18n',
-        component: () => import('@/views/i18n.vue')
-      },
-      {
-        path: '/mock',
-        component: () => import('@/views/mock.vue')
-      },
-      // 自定义组件
-      {
-        path: '/button',
-        component: () => import('@/views/myElement/ButtonExample.vue')
-      },
-      {
-        path: '/input',
-        component: () => import('@/views/myElement/InputExample.vue')
-      },
-      {
-        path: '/intinput',
-        component: () => import('@/views/myElement/IntInputExample.vue')
-      },
-      {
-        path: '/decimalinput',
-        component: () => import('@/views/myElement/DecimalInputExample.vue')
-      },
-    ]
-  },
-  // {
-  //   path: '/about',
-  //   name: 'About',
-  //   component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  // }
-]
+let routes = []
+const files = require.context('./modules', true, /\.js$/)
+
+files.keys().forEach(key => {
+  routes = routes.concat(files(key).default)
+});
+
+routes.sort((a, b)=>{
+  return a.index - b.index
+})
 
 const router = new VueRouter({
   routes
