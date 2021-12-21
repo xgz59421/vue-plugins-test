55f182af0a34a11158770c5e17e6f982512cdba9
zh <87256657@qq.com>
2021-12-21 10:52:38 +0800

    echarts toolbox

--- src/views/echarts/line.vue
+++ src/views/echarts/line.vue
@@ -17,9 +17,18 @@
         let main = document.getElementById("main");
         let myechart = echarts.init(main);
         let option = {
+          dataset: {
+            source: [
+              ['姓名', 'data1', 'data2', 'data3', 'data4'],
+              ['坂井泉水', 30, 83, 73, 55],
+              ['葵司', 21, 43, 85, 93],
+              ['麻生希', 16, 86, 65, 82],
+              ['新垣结衣', 11, 72, 53, 39]
+            ]
+          },
           title: [{
               text: 'titlexxxxxx',
-              x: 'center',
+              x: 'left',
               top: '10',
               textStyle: {
                 color: '#049783'
@@ -31,24 +40,50 @@
               y: 'center'
             }
           ],
-          xAxis: {
-            type: 'category',
-            data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
-          },
-          yAxis: {
-            type: 'value'
-          },
-          tooltip: {
-            trigger: 'axis',
-            axisPointer: {
-              type: 'line'
-            },
-            formatter: function (params) {
-              for (let i = 0; i < params.length; i++) {
-                return 'xxx:' + params[i].name + '--yyy:' + params[i].data
-              }
+          // tooltip: {
+          //   trigger: 'axis'
+          // },
+          legend: {},
+          toolbox: {
+            show: true,
+            feature: {
+              // dataZoom: {
+              //   yAxisIndex: 'none'
+              // },
+              dataView: {
+                readOnly: true,
+                optionToContent: function(opt) {
+                  console.log(opt);
+                    var source = opt.dataset[0].source
+                    let dataset = opt.dataset
+                    var table = '<table border="1" style="width:100%;text-align:center"><tbody>'
+                    for (let i = 0; i < source.length; i++) {
+                      let array = source[i];
+                      table += '<tr>'
+                      for (let j = 0; j < array.length; j++) {
+                        let item = array[j];
+                        table += `<td style="color: #f00">${item}</td>`
+                      }
+                      table += '</tr>'
+                    }                    
+                    table += '</tbody></table>';
+                    return table;
+                }
+              },
+              magicType: {
+                type: ['line', 'bar']
+              },
+              restore: {},
+              saveAsImage: {}
             }
           },
+          xAxis: [{
+            type: 'category',
+            gridIndex: 0
+          }],
+          yAxis: [{
+            gridIndex: 0
+          }],
           dataZoom: [{
               type: 'slider',
               start: 0,
@@ -60,12 +95,33 @@
               end: 100
             }
           ],
-          series: [{
-            data: [150, 230, 224, 218, 135, 147, 260],
-            type: 'line',
-            color: '#1690d8'
-          }]
-        }
+          series: [
+            {
+              type: 'line',
+              serierLayoutBy: 'row',
+              smooth: true,
+              symbol: 'none'
+            },
+            {
+              type: 'line',
+              serierLayoutBy: 'row',
+              smooth: true,
+              symbol: 'none'
+            },
+            {
+              type: 'line',
+              serierLayoutBy: 'row',
+              smooth: true,
+              symbol: 'none'
+            },
+            {
+              type: 'line',
+              serierLayoutBy: 'row',
+              smooth: true,
+              symbol: 'none'
+            },
+          ]
+        };
         myechart.setOption(option);
       }
     },
