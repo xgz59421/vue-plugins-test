4d14691c4853d2cc13acfe2cdf438cb60d6dcbc7
zh <87256657@qq.com>
2022-06-11 17:41:28 +0800

    1

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,9 @@
 const path = require('path')
-
+console.log('process.env.NODE_ENV', process.env.NODE_ENV)
+const isDev = process.env.NODE_ENV === 'dev'
+console.log('isDev', isDev)
 module.exports = {
+  publicPath: isDev ? '/' : '/vue-plugins-test',
   configureWebpack: {
     resolve: {
       alias: {
